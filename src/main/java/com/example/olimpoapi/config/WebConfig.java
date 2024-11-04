8cdf5432a795c1acdc5f0c5b7298a006868f4c4e
RaphaCosil <Raphael.Cosil@gmail.com>
2024-11-04 10:53:05 -0300

    ADD - Permitindo o CORS em todas rotas

---
+++ src/main/java/com/example/olimpoapi/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.example.olimpoapi.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite CORS em todas as rotas
+        .allowedOrigins("*") // Permite todas as origens
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
