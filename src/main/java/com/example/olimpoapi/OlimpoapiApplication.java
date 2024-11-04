64ab3997cf522c10a2e14c966ed0fc83c17f68dc
Joaquim <124395828+JoaquimSilvaSousa@users.noreply.github.com>
2024-11-04 12:37:16 -0300

    Update OlimpoapiApplication.java

--- src/main/java/com/example/olimpoapi/OlimpoapiApplication.java
+++ src/main/java/com/example/olimpoapi/OlimpoapiApplication.java
@@ -3,9 +3,19 @@ package com.example.olimpoapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @SpringBootApplication
-public class OlimpoapiApplication {
+public class OlimpoapiApplication implements WebMvcConfigurer {
     public static void main(String[] args) {
         SpringApplication.run(OlimpoapiApplication.class, args);
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite todas as rotas
+                .allowedOrigins("*") // Permite todas as origens
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*"); // Cabeçalhos permitidos
     }
 }
