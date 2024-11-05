f194c8face62f0c31ca4054437b3c2cdec77b2d0
Joaquim <124395828+JoaquimSilvaSousa@users.noreply.github.com>
2024-11-05 08:31:57 -0300

    Update WebConfig.java

--- src/main/java/com/example/olimpoapi/config/WebConfig.java
+++ src/main/java/com/example/olimpoapi/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://127.0.0.1:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowCredentials(true);
     }
 }
